d3ced7d1b74fb2c04e7161212383874a4de4cceb
Prasanna SM <114782548+smprasanna@users.noreply.github.com>
2026-08-02 01:32:23 -0400

    Add: python-type-errors - submission-0

---
+++ Python For Beginners/python-type-errors/submission-0.py
@@ -0,0 +1,17 @@
+variable = "10"
+
+# Don't modify the code below this line
+print(variable)
+print(type(variable))
+
+variable = int(variable)
+
+print(variable)
+print(type(variable))
+
+# ValueError is a runtime error. It occurs during the execution of a program, 
+# after the code has been successfully parsed and has started running. It happens 
+# when a function or operation receives an argument of the correct type but an 
+# inappropriate value for the specific operation being performed. 
+# An example is trying to convert the string "hello" to an integer using 
+# int("hello").
