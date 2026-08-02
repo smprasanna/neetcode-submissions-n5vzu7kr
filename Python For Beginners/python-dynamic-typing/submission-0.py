628cf3022db90c5ed0cedd68e38cc1be878c4e5b
Prasanna SM <114782548+smprasanna@users.noreply.github.com>
2026-08-02 00:45:55 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 7
+print(type(variable))
+variable = 7.7
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "seven"
+print(type(variable))
+variable = [7,7.7]
+print(type(variable))
